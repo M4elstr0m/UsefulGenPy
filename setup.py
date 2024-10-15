bf50885da55e750266351ed6e4e55de5fa56be0d
M4elstr0m <112475171+M4elstr0m@users.noreply.github.com>
2024-10-15 22:07:02 +0200

    Added every files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='usefulgen',
+    version='1.0',
+    packages=['usefulgen'],
+    install_requires=['typing'],
+    author='M4elstr0m',
+    description='A Python module containing some must-have generators',
+    url='https://github.com/M4elstr0m/UsefulGenPy',  # Optional
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # Choose a license
+    ],
+)
