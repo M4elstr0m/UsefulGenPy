02ec7ba2e1961064be256517a89933a3f1ed7977
M4elstr0m <4ft3rL1fe@proton.me>
2024-10-16 17:08:52 +0200

    v1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='usefulgen',
-    version='1.0.0',
+    version='1.0.1',
     packages=['usefulgen'],
     description='A Python script containing some must-have generators',
     install_requires=['typing'],
