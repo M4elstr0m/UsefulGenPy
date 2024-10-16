a6c173ec965fab6f5cbba5007803f667c06ac1f4
M4elstr0m <4ft3rL1fe@proton.me>
2024-10-16 17:06:10 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ setup(
     name='usefulgen',
     version='1.0.0',
     packages=['usefulgen'],
+    description='A Python script containing some must-have generators',
     install_requires=['typing'],
     author='M4elstr0m',
-    description='A Python module containing some must-have generators',
-    url='https://github.com/M4elstr0m/UsefulGenPy',  # Optional
+    long_description='A Python module containing some must-have generators\nFor more details, don\'t hesitate to go on https://github.com/M4elstr0m/UsefulGenPy',
+    url='https://github.com/M4elstr0m/UsefulGenPy',
+    download_url='https://pypi.org/project/usefulgen/',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',  # Choose a license
