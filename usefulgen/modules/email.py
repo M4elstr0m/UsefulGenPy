de7ac9f4a5acb41a2104d1ff27ba4e74095ccb8f
M4elstr0m <4ft3rL1fe@proton.me>
2024-10-19 18:14:45 +0200

    Modified email.py

--- usefulgen/modules/email.py
+++ usefulgen/modules/email.py
@@ -16,12 +16,24 @@ def EmailGenerate(
     Generates all possible emails using the given informations
     """
     # Type checking
-    for parameter in [firstname,lastname,nationality,domain,birthday_year,pseudonym,nickname,pet]:
-        if not isinstance(parameter, str) and parameter is not None:
-            raise TypeError(f"Expected string-type as argument received \"{parameter}\"")
+    for string_parameter in [firstname,lastname,nationality,domain,birthday_year,pseudonym,nickname,pet]:
+        if not isinstance(string_parameter, str) and string_parameter is not None:
+            raise TypeError(f"Expected string-type as argument: received \"{string_parameter}\"")
     
+    # Convert all to lowercase
+    firstname = str.lower(firstname) if firstname is not None else None
+    lastname = str.lower(lastname) if lastname is not None else None
+    nationality = str.lower(nationality) if nationality is not None else None
+    domain = str.lower(domain) if domain is not None else None
+    birthday_year = str.lower(birthday_year) if birthday_year is not None else None
+    pseudonym = str.lower(pseudonym) if pseudonym is not None else None
+    nickname = str.lower(nickname) if nickname is not None else None
+    pet = str.lower(pet) if pet is not None else None
+
     def __DictDomainsByCountry() -> dict:
-        """Created to save memory when executing code"""
+        """
+        Stores the DomainsByCountry dictionnary, ready to be applied to a variable. Created to save memory when executing code
+        """
         DomainsByCountry:dict = { # Domain only OR all + nationality if there is no Domain
             "_all":["@gmail.com", "@outlook.com", "@yahoo.com", "@hotmail.com", "@icloud.com", "@live.com", "@proton.me", "@protonmail.com"],
             "at":["@gmx.at", "@aon.at", "@live.at", "@outlook.at"],
@@ -66,6 +78,40 @@ def EmailGenerate(
         }
         return DomainsByCountry
 
+    # Value checking
+    if domain is not None and not domain.startswith("@"):
+        raise ValueError(f"`domain` must start with a \"@\" to be functional")
+    elif nationality is not None and nationality not in list(__DictDomainsByCountry().keys()):
+         raise ValueError(f"`nationality` must be a country code (eg: US, UK...) known by this program")
 
-if __name__ == "__main__":
-    EmailGenerate("John","Doe")
+    # Defines all the possible domains depending on the given parameters
+    PossibleDomains:list = ([domain,]) if domain is not None else (__DictDomainsByCountry()[nationality]+__DictDomainsByCountry()["_all"]) if nationality is not None else (__DictDomainsByCountry()["_all"])
+    AlreadyYield:list = []
+
+    if firstname is not None and lastname is not None:
+        for do in PossibleDomains:
+            if firstname+lastname+do not in AlreadyYield:
+                AlreadyYield.append(firstname+lastname+do)
+                yield firstname+lastname+do
+            if firstname+"."+lastname+do not in AlreadyYield:
+                AlreadyYield.append(firstname+"."+lastname+do)
+                yield firstname+"."+lastname+do
+            if firstname+"_"+lastname+do not in AlreadyYield:
+                AlreadyYield.append(firstname+"_"+lastname+do)
+                yield firstname+"_"+lastname+do
+            if firstname+"_"+lastname+do not in AlreadyYield:
+                AlreadyYield.append(lastname+firstname+do)
+                yield lastname+firstname+do
+
+                ### sort depending on if lastname is the first one or if it is firstname in first
+                yield lastname+"."+firstname+do
+                yield lastname+"_"+firstname+do
+                yield firstname+do
+                yield lastname+do
+                yield lastname+"-"+firstname+do
+                yield firstname+"-"+lastname+do
+                
+
+if __name__ == "__main__": #to delete
+    for i in EmailGenerate(firstname="John",lastname="Doe"):
+        print(i)
